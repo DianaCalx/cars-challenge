ec2f2dbd28938c37345984cdf6d1b57c11290d47
Diana Calderon <dianacalderon@ravn.co>
2022-08-30 14:43:57 -0600

    Create custom useSearchParams hook

---
+++ src/hooks/useSearchParams.ts
@@ -0,0 +1,23 @@
+import { useSearchParams as useSearchParamsRRD } from 'react-router-dom';
+
+export const useSearchParams = () => {
+  const [search, setSearch] = useSearchParamsRRD();
+
+  const searchParams = Object.fromEntries(search);
+
+  const setSearchParam = (key: string, value: string) => {
+    setSearch({ ...searchParams, [key]: value })
+  }
+
+  const removeSearchParam = (key: string) => {
+    const newSearchParams = { ...searchParams }
+    delete newSearchParams[key]
+    setSearch({ ...newSearchParams })
+  }
+
+  return {
+    searchParams,
+    setSearchParam,
+    removeSearchParam
+  };
+}
