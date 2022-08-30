1373b67d2a512278688566f954a99b58e35eff94
Diana Calderon <dianacalderon@ravn.co>
2022-08-30 14:33:23 -0600

    Config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import CarForm from './pages/CarForm';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/car-form' element={<CarForm/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
