5ca0a88ea95e151b69774969a3617ccaa7b611df
Diana Calderon <dianacalderon@ravn.co>
2022-09-01 15:56:05 -0600

    Restructure the home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,7 @@
 import { useSearchParams } from '../hooks/useSearchParams';
 import styled from 'styled-components';
 import Login from '../components/Login';
-import { useCarContext } from '../context/carContext';
-import useLocalStorage from '../hooks/useLocalStorage';
+import Header from '../components/Header';
 
 const HomePage = styled.div`
   background-color: ${props => props.theme.colors.mainColor};
@@ -10,24 +9,6 @@ const HomePage = styled.div`
   padding: 1.5rem;
   display: flex;
   flex-direction: column;
-`;
-
-const Buttons = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  gap: 1rem;
-`;
-
-const Button = styled.button`
-  background-color: ${props => props.theme.colors.darkColor};
-  letter-spacing: 0.1rem;
-  color: white;
-  border: 1px solid black;
-  cursor: pointer;
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  padding: 1rem 2rem;
 `;
 
 const Greeting = styled.div`
@@ -41,26 +22,12 @@ const Greeting = styled.div`
 `;
 
 const Home = () => {
-  const { searchParams, setSearchParam} = useSearchParams();
-  const { user, setUser } = useCarContext();
-  const { removeLS } = useLocalStorage('user');
-
-  const onSubmitLogout = () => {
-    removeLS();
-    setUser(undefined);
-  }
+  const { searchParams } = useSearchParams();
 
   return (
     <HomePage>
       {searchParams.login === 'true' && <Login />}
-      <Buttons>
-        { user && <Button>My Favorites</Button>}
-        <Button>Cars</Button>
-        { user 
-          ? <Button onClick={onSubmitLogout}>Logout</Button>
-          : <Button onClick={() => setSearchParam('login', 'true')}>Login</Button>
-        }       
-      </Buttons>
+      <Header />
       <Greeting>Welcome</Greeting>
     </HomePage>
   )
