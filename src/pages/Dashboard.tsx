a7e12ebade4130e40ab01a1f1da7fa9e6a95f776
Diana Calderon <dianacalderon@ravn.co>
2022-09-01 15:53:58 -0600

    Config Dashboard Page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import styled from 'styled-components';
+import CarsList from '../components/CarsList';
+import Header from '../components/Header'
+import Login from '../components/Login';
+import { useSearchParams } from '../hooks/useSearchParams';
+
+const DashboardPage = styled.div`
+  background-color: ${props => props.theme.colors.mainColor};
+  height: 100vh;
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Dashboard = () => {
+  
+  const { searchParams } = useSearchParams();
+
   return (
-    <div>Dashboard</div>
+    <DashboardPage>
+      {searchParams.login === 'true' && <Login />}
+      <Header />
+      <CarsList />
+    </DashboardPage>
   )
 }
 
