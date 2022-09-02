35f93cf35e18ef7c5937023c82eb9da299be995f
Diana Calderon <dianacalderon@ravn.co>
2022-09-02 09:14:02 -0600

    Create Description Component

---
+++ src/components/Description.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+
+const ImageCarDesc = styled.div`
+  width: 20%;
+`;
+
+const InformationLotDesc = styled.div`
+  width: 30%;
+`;
+
+const DescriptionVehDesc = styled.div`
+  width: 15%;
+`;
+
+const ConditionDesc = styled.div`
+  width: 15%;
+`;
+
+const SalesDesc = styled.div`
+  width: 20%;
+`;
+
+const SelectButtonDesc = styled.div`
+  width: 5%;
+`;
+
+const Description = () => {
+  return (
+    <Container>
+      <ImageCarDesc>Image</ImageCarDesc>
+      <InformationLotDesc>Lot Information</InformationLotDesc>
+      <DescriptionVehDesc>Vehicle Information</DescriptionVehDesc>
+      <ConditionDesc>Condition</ConditionDesc>
+      <SalesDesc>Sales Information</SalesDesc>    
+      <SelectButtonDesc>Others</SelectButtonDesc>
+    </Container>
+  )
+}
+
+export default Description
