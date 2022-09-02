55f9548a551ebb08c6e744a6b3f7f78b9b8f8e77
Diana Calderon <dianacalderon@ravn.co>
2022-09-02 11:16:40 -0600

    Add Description Component and Modify the styles

--- src/components/CarsList.tsx
+++ src/components/CarsList.tsx
@@ -1,14 +1,16 @@
 import { useCarsQuery } from '../generated/graphql';
 import CarCard from './CarCard';
 import { Cars } from '../generated/graphql';
+import Description from './Description';
 
 const CarsList = () => {
   const { loading, data, error } = useCarsQuery();
 
   return (
     <div>
+      <Description/>
       {data?.cars?.map(car => 
-        <CarCard key={car.id} car={car as Cars}/>
+        <CarCard key={car.id} car={car as Cars}/>   
       )}
     </div>
   )
--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
   gap: 1.5rem;
+  align-items: center;
+  background-color: black;
+  color: white;
+  padding: 1rem 0;
+  border-radius: 0.5rem;
+  margin-bottom: 0.5rem;
 `;
 
 const ImageCarDesc = styled.div`
@@ -13,16 +19,16 @@ const InformationLotDesc = styled.div`
   width: 30%;
 `;
 
+const SalesDesc = styled.div`
+  width: 20%;
+`;
+
 const DescriptionVehDesc = styled.div`
   width: 15%;
 `;
 
 const ConditionDesc = styled.div`
-  width: 15%;
-`;
-
-const SalesDesc = styled.div`
-  width: 20%;
+  width: 10%;
 `;
 
 const SelectButtonDesc = styled.div`
