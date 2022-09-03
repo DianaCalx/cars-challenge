c257592b18c2522c07edb96df25850653274ebfd
Diana Calderon <dianacalderon@ravn.co>
2022-09-03 15:58:23 -0600

    Use Button Component reusable in Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,28 @@ import { useAppContext } from '../context/appContext';
 import useLocalStorage from '../hooks/useLocalStorage';
 import { useNavigate, useLocation} from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Button from './Button';
 
-const Buttons = styled.div`
-  width: 100%;
+const HeaderContainer = styled.header`
   display: flex;
-  justify-content: flex-end;
-  gap: 1rem;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
   margin-bottom: 2rem;
 `;
 
+const UserName = styled.p`
+  margin: 0;
+`;
 
-const Button = styled.button`
+const Buttons = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  gap: 1rem;
+`;
+
+const StyledButton = styled.button`
   min-width: 13rem;
   height: 4rem;
   padding: 0.5rem 1rem;
@@ -54,15 +65,18 @@ const Header = () => {
   }
 
   return (
-    <Buttons>
-      { createButton && <Button onClick={() => navigate('/car-form')}>Create Car</Button>}
-      { user && <Button>My Favorites</Button>}
-      <Button onClick={() => navigate('/dashboard')}>Cars</Button>
-      { user 
-        ? <Button onClick={onSubmitLogout}>Logout</Button>
-        : <Button onClick={()=> setIsLoginModalOpen(true)}>Login</Button>
-      }       
+    <HeaderContainer>
+      {user && <UserName>{`Welcome ${user.first_name} ${user.last_name}!`}</UserName>}
+      <Buttons>
+        { createButton && <Button StyledButton={StyledButton} onClick={() => navigate('/car-form')}>Create Car</Button>}
+        { user && <Button StyledButton={StyledButton} onClick={() => navigate('/favorites')}>My Favorites</Button>}
+        <Button StyledButton={StyledButton} onClick={() => navigate('/dashboard')}>Cars</Button>
+        { user 
+          ? <Button StyledButton={StyledButton} onClick={onSubmitLogout}>Logout</Button>
+          : <Button StyledButton={StyledButton} onClick={()=> setIsLoginModalOpen(true)}>Login</Button>
+        }       
     </Buttons>
+    </HeaderContainer>
   )
 }
 
