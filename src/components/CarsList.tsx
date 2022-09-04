fbdb6bf8ec722be019aab3244186c9ced61bde23
Diana Calderon <dianacalderon@ravn.co>
2022-09-04 10:33:22 -0600

    Update CarList to display cars

--- src/components/CarsList.tsx
+++ src/components/CarsList.tsx
@@ -2,15 +2,42 @@ import { useCarsQuery } from '../generated/graphql';
 import CarCard from './CarCard';
 import { Cars } from '../generated/graphql';
 import Description from './Description';
+import { useAppContext } from '../context/appContext';
+import { useEffect, useState } from 'react';
+
+export interface CarItem extends Cars {
+  isFavorite: boolean
+}
 
 const CarsList = () => {
-  const { loading, data, error } = useCarsQuery();
+  const [cars, setCars] = useState<CarItem[]>([]);
+  const { user } = useAppContext();
+  const { loading, data, error } = useCarsQuery({
+    variables: {
+      where: {
+        user_id: {
+          _eq: user?.id || 0
+        }
+      }
+    }
+  });
+
+  useEffect(() => {
+    if (data?.cars) {
+      const carsWithFavoriteAttribute = data.cars.map(car => ({
+        ...car,
+        isFavorite: data.user_cars.some(userCar => userCar.car_id === car.id)
+      }));
+
+      setCars(carsWithFavoriteAttribute as CarItem[]);
+    }
+  }, [data]);
 
   return (
-    <div>
+    <div style={{ backgroundColor: '#eee' }}>
       <Description/>
-      {data?.cars?.map(car => 
-        <CarCard key={car.id} car={car as Cars}/>   
+      {cars.map(car => 
+        <CarCard key={car.id} car={car} setCars={setCars} />   
       )}
     </div>
   )
