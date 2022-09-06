126ce1b0949d390e50c1d2ec0ff4529e9749b45b
Diana Calderon <dianacalderon@ravn.co>
2022-09-06 10:25:40 -0600

    Evaluate path in Dashboard component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,14 @@
+import { Navigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import CarsList from '../components/CarsList';
 import Header from '../components/Header'
 import Login from '../components/Login';
 import { useAppContext } from '../context/appContext';
+import useLocalStorage from '../hooks/useLocalStorage';
+import Filters from '../components/Filters';
 
 const DashboardPage = styled.div`
-  background-color: ${props => props.theme.colors.secondaryColor};
+  background-color: ${props => props.theme.colors.mainColor};
   min-height: 100%;
   padding: 1.5rem;
   display: flex;
@@ -13,12 +16,19 @@ const DashboardPage = styled.div`
 `;
 
 const Dashboard = () => {
-  const { isLoginModalOpen } = useAppContext();
+  const { isLoginModalOpen, user } = useAppContext();
+  const { getLocalStorage } = useLocalStorage();
+  const { pathname } = useLocation();
+ 
+  if (!user && pathname === '/favorites' && !getLocalStorage('user')) {
+    return <Navigate to="/dashboard" replace />;
+  }
 
   return (
     <DashboardPage>
       {isLoginModalOpen && <Login />}
       <Header />
+      <Filters/>
       <CarsList />
     </DashboardPage>
   )
