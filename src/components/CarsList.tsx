b979f64433bae7d2528aa8775322c3ba09655a5d
Diana Calderon <dianacalderon@ravn.co>
2022-09-06 10:24:34 -0600

    Evaluate path in CarList Component

--- src/components/CarsList.tsx
+++ src/components/CarsList.tsx
@@ -4,14 +4,23 @@ import { Cars } from '../generated/graphql';
 import Description from './Description';
 import { useAppContext } from '../context/appContext';
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
 export interface CarItem extends Cars {
   isFavorite: boolean
 }
 
+
+const CarListContainer = styled.div`
+  background-color: ${props => props.theme.colors.neutralColor};
+  border-radius: 0.5rem;
+`;
 const CarsList = () => {
   const [cars, setCars] = useState<CarItem[]>([]);
+  const { pathname } = useLocation();
   const { user } = useAppContext();
+
   const { loading, data, error } = useCarsQuery({
     variables: {
       where: {
@@ -34,12 +43,17 @@ const CarsList = () => {
   }, [data]);
 
   return (
-    <div style={{ backgroundColor: '#eee' }}>
+    <CarListContainer>
       <Description/>
-      {cars.map(car => 
-        <CarCard key={car.id} car={car} setCars={setCars} />   
-      )}
-    </div>
+      {pathname === '/favorites'
+        ? cars.filter(car => car.isFavorite).map(car => 
+            <CarCard key={car.id} car={car} setCars={setCars} />   
+          )
+        : cars.map(car => 
+            <CarCard key={car.id} car={car} setCars={setCars} />   
+          )
+      }
+    </CarListContainer>
   )
 }
 
