d061874b21f78575e4dc9412ae33e5f3113db92b
Diana Calderon <dianacalderon@ravn.co>
2022-09-08 13:24:53 -0600

    Update styles in Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,12 @@ const HeaderContainer = styled.header`
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  margin-bottom: 2rem;
 `;
 
 const UserName = styled.p`
   margin: 0;
+  color: white;
+  font-size: 1.8rem;
 `;
 
 const Buttons = styled.div`
@@ -33,12 +34,26 @@ const StyledButton = styled.button`
   cursor: pointer;
   border: 2px solid ${props => props.theme.colors.darkColor};
   border-radius: 0.5rem;
-  background: ${props => props.theme.colors.mainColor}; 
-  color: ${props => props.theme.colors.darkColor2}; 
+  background: ${props => props.theme.colors.darkColor}; 
 
   &:hover{
-    background: ${props => props.theme.colors.darkColor};
-    color: white;
+    background: ${props => props.theme.colors.darkColor2};
+  }
+`;
+const LogButton = styled.button`
+  min-width: 13rem;
+  height: 4rem;
+  padding: 0.5rem 1rem;
+  font-size: 1.7rem;
+  font-weight: bold;
+  cursor: pointer;
+  border: 2px solid ${props => props.theme.colors.successColor};
+  border-radius: 0.5rem;
+  background: ${props => props.theme.colors.successColor}; 
+  color: white;
+
+  &:hover{
+    background: ${props => props.theme.colors.successColor2};
   }
 `;
 const Header = () => {
@@ -73,8 +88,8 @@ const Header = () => {
         { user && <Button StyledButton={StyledButton} onClick={() => navigate('/favorites')}>My Favorites</Button>}
         <Button StyledButton={StyledButton} onClick={() => navigate('/dashboard')}>Cars</Button>
         { user 
-          ? <Button StyledButton={StyledButton} onClick={onSubmitLogout}>Logout</Button>
-          : <Button StyledButton={StyledButton} onClick={()=> setIsLoginModalOpen(true)}>Login</Button>
+          ? <Button StyledButton={LogButton} onClick={onSubmitLogout}>Logout</Button>
+          : <Button StyledButton={LogButton} onClick={()=> setIsLoginModalOpen(true)}>Login</Button>
         }       
     </Buttons>
     </HeaderContainer>
