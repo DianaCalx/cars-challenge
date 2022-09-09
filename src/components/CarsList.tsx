1e63b689973421781d05e05fa361e5982eb108a3
Diana Calderon <dianacalderon@ravn.co>
2022-09-09 08:46:22 -0600

    Add the spinner and change the logic to show the cars in the CarList component

--- src/components/CarsList.tsx
+++ src/components/CarsList.tsx
@@ -4,26 +4,22 @@ import { Cars } from '../generated/graphql';
 import Description from './Description';
 import { useAppContext } from '../context/appContext';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Filters, { FilterFormInputs } from './Filters';
 import { validate } from 'uuid';
-
+import Spinner from './Spinner';
 export interface CarItem extends Cars {
   isFavorite: boolean
 }
-
 
 const CarListContainer = styled.div`
   background-color: ${props => props.theme.colors.neutralColor};
   border-radius: 0.5rem;
 `;
 const CarsList = () => {
-  const [cars, setCars] = useState<CarItem[]>([]);
   const [filters, setFilters] = useState<FilterFormInputs>();
-  const { pathname } = useLocation();
   const { user } = useAppContext();
-
+  const [favorites, setFavorites] = useState<number[]>([]);
 
   const { loading, data, error, refetch } = useCarsQuery({
     variables: {
@@ -32,7 +28,8 @@ const CarsList = () => {
           _eq: user?.id || 0
         }
       }
-    }
+    },
+    fetchPolicy: 'no-cache'
   });
 
   useEffect(() => {
@@ -70,30 +67,27 @@ const CarsList = () => {
   }, [filters, refetch, user?.id]);
 
   useEffect(() => {
-    if (data?.cars) {
-      const carsWithFavoriteAttribute = data.cars.map(car => ({
-        ...car,
-        isFavorite: data.user_cars.some(userCar => userCar.car_id === car.id)
-      }));
-
-      setCars(carsWithFavoriteAttribute as CarItem[]);
+    if (data?.user_cars) {
+      const favoriteCars = data.user_cars.map(favoriteCar => favoriteCar.car_id);
+      setFavorites(favoriteCars);
     }
-  }, [data]);
+  }, [data?.user_cars]);
 
   return (
     <>
-    <Filters setFilters={setFilters} />
-    <CarListContainer>    
-      <Description/>
-      {pathname === '/favorites'
-        ? cars.filter(car => car.isFavorite).map(car => 
-            <CarCard key={car.id} car={car} setCars={setCars} />   
-          )
-        : cars.map(car => 
-            <CarCard key={car.id} car={car} setCars={setCars} />   
-          )
-      }
-    </CarListContainer>
+      <Filters setFilters={setFilters} />
+      <CarListContainer>    
+        <Description/>
+        {loading && <Spinner/>}
+        {error && <p>Was an error</p>}
+        {data?.cars.map(car => {
+          const carWithFavorite = {
+            ...car,
+            isFavorite: favorites.includes(car.id)
+          }
+          return <CarCard key={car.id} car={carWithFavorite as CarItem} refetchCars={refetch} />   
+        })}
+      </CarListContainer>
     </>
   )
 }
