3ae400477b7f6ffd83821350d93c40f9031747c7
Diana Calderon <dianacalderon@ravn.co>
2022-09-10 16:42:30 -0600

    Change filters logic and update styles in Filters Component

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import { useSearchParams } from '../hooks/useSearchParams';
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import styled from 'styled-components';
@@ -8,7 +9,7 @@ const FormFilter = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 2rem;
-  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 60%, rgba(0,95,255,1) 100%);
+  background: ${props => props.theme.gradient};
   padding: 0 2rem 2rem 1rem;
 `;
 
@@ -45,25 +46,24 @@ export interface FilterFormInputs {
   sale_date?: string
 }
 
-interface FiltersProps {
-  setFilters: React.Dispatch<React.SetStateAction<FilterFormInputs | undefined>>
-}
-
 const schema = Yup.object().shape({
   search: Yup.string(),
   sale_date: Yup.string(),
 });
 
-const Filters = ({ setFilters }: FiltersProps) => {
-
-  const { register, handleSubmit, formState: { errors } } = useForm<FilterFormInputs>({resolver: yupResolver(schema)});
+const Filters = () => {
+  const { setSearchParam } = useSearchParams();
+  const { register, handleSubmit } = useForm<FilterFormInputs>({resolver: yupResolver(schema)});
 
   const onSubmit = (data: FilterFormInputs) => {
-    setFilters(data);
+      setSearchParam({
+        search: data.search || '',
+        sort: data.sale_date || '',
+      });
   };
 
   return (
-    <FormFilter>
+    <FormFilter onSubmit={handleSubmit(onSubmit)}>
       <SearchBarContainer>
         <Filter type="text" {...register("search")} ></Filter>
         <Button StyledButton={ButtonSearch} type="submit" onClick={handleSubmit(onSubmit)}>Search Inventory</Button>
