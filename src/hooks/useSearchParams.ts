76fe6838fc6684e08df9b750259423585f8e29be
Diana Calderon <dianacalderon@ravn.co>
2022-09-10 16:47:51 -0600

    Refactor useSearchParams hook

--- src/hooks/useSearchParams.ts
+++ src/hooks/useSearchParams.ts
@@ -1,12 +1,24 @@
 import { useSearchParams as useSearchParamsRRD } from 'react-router-dom';
 
+interface searchParams {
+  [key: string]: string
+}
+
 export const useSearchParams = () => {
   const [search, setSearch] = useSearchParamsRRD();
 
   const searchParams = Object.fromEntries(search);
 
-  const setSearchParam = (key: string, value: string) => {
-    setSearch({ ...searchParams, [key]: value });
+  const setSearchParam = (params: searchParams) => {
+    const newParams = { ...searchParams, ...params };
+
+    for (let propName in newParams) { 
+      if (!newParams[propName]) {
+        delete newParams[propName];
+      }
+    }
+
+    setSearch(newParams);
   }
 
   const removeSearchParam = (key: string) => {
