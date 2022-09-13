9b5280fdc6cab7b1e10d2babc7cf0927308e7f38
Diana Calderon <dianacalderon@ravn.co>
2022-09-13 11:13:22 -0600

    Create Favorites Component

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import { useCarsQuery } from '../generated/graphql';
+import CarCard from '../components/CarCard';
+import { Cars } from '../generated/graphql';
+import Description from '../components/Description';
+import { useAppContext } from '../context/appContext';
+import Filters from '../components/Filters';
+import Spinner from '../components/Spinner';
+import styled from 'styled-components';
+import { useSearchParams } from 'react-router-dom';
+import { getVariablesQueryCars } from '../utils/getVariablesQueryCars';
+
+export interface CarItem extends Cars {
+  isFavorite: boolean
+};
+
+const FavoritesContainer = styled.div`
+  background-color: ${props => props.theme.colors.neutralColor};
+  border-radius: 0.5rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Error = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 2rem;
+`;
+
+const NotFound = styled.p`
+  color:  ${props => props.theme.colors.successColor2};
+  font-weight: bold;
+  font-size: 2rem;
+  text-align: center;
+`;
+
+const Favorites = () => {
+  
+  const { user } = useAppContext();
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [search] = useSearchParams();
+  const variables = getVariablesQueryCars(search, user?.id);
+
+  const { loading, data, error, refetch } = useCarsQuery({
+    variables,
+    fetchPolicy: 'no-cache'
+  });
+
+  useEffect(() => {
+    if (data?.user_cars) {
+      const favoriteCars = data.user_cars.map(favoriteCar => favoriteCar.car_id);
+      setFavorites(favoriteCars);
+    }
+  }, [data?.user_cars]);
+
+  return (
+    <>
+      <Filters/>
+      <FavoritesContainer>    
+        <Description/>
+        {error && <Error>There was an error</Error>}
+        {loading ?  <Spinner/> 
+        : data?.cars.map(car => {
+            const carWithFavorite = {
+              ...car,
+              isFavorite: favorites.includes(car.id)
+            }
+            if(carWithFavorite.isFavorite){
+              return <CarCard key={car.id} car={carWithFavorite as CarItem} refetchCars={refetch} />
+            }
+            return null;
+          })}
+          {
+            (!favorites.length && !loading )  && <NotFound>Favorite cars were not found</NotFound>
+          }     
+      </FavoritesContainer>
+    </>
+  )
+}
+
+export default Favorites
