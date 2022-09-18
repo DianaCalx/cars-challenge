9a2cb749c78fb035902cc0e71aa057a1081be75b
Diana Calderon <dianacalderon@ravn.co>
2022-09-18 09:08:21 -0600

    Add favorites query to Dashboard Component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 import CarsList from '../components/CarsList';
 import Header from '../components/Header'
@@ -6,6 +6,9 @@ import Login from '../components/Login';
 import { useAppContext } from '../context/appContext';
 import useLocalStorage from '../hooks/useLocalStorage';
 import Favorites from '../components/Favorites';
+import { useEffect } from 'react';
+import { getVariablesQueryCars } from '../utils/getVariablesQueryCars';
+import { useFavoritesLazyQuery } from '../generated/graphql';
 
 const DashboardPage = styled.div`
   display: flex;
@@ -20,9 +23,34 @@ const DashboarHeader = styled.div`
 `;
 
 const Dashboard = () => {
-  const { isLoginModalOpen, user } = useAppContext();
+  const { isLoginModalOpen, user, setFavorites } = useAppContext();
+  const [search] = useSearchParams();
+  const { whereUserCars} = getVariablesQueryCars(search, user?.id);  
   const { getLocalStorage } = useLocalStorage();
   const { pathname } = useLocation();
+  const [refectchFavorites,  { data: dataFavorites}] = useFavoritesLazyQuery();
+
+  useEffect(() => {
+    if(user){
+      refectchFavorites({
+        variables: {
+          whereUserCars
+        }
+      });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  
+  useEffect(() => {
+    if (dataFavorites?.user_cars) {
+      const favoriteCars = dataFavorites.user_cars.map(favoriteCar => favoriteCar.car_id);
+      setFavorites(favoriteCars);
+    } else{
+      setFavorites([]);
+    }
+  }, [dataFavorites?.user_cars, setFavorites, user]);
+
  
   if (!user && pathname === '/favorites' && !getLocalStorage('user')) {
     return <Navigate to="/dashboard" replace />;
