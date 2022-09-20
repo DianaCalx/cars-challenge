24cf5662fcfdae9ec714b39d3d91a30dd1c04f73
Diana Calderon <dianacalderon@ravn.co>
2022-09-19 22:13:52 -0600

    Add switch to validate condition in condition component

--- src/components/Condition.tsx
+++ src/components/Condition.tsx
@@ -2,10 +2,19 @@ interface ConditionProp {
   condition: string;
 }
 
-const Condition = ({ condition }: ConditionProp) => (
-  <div>
-    {condition === 'A' ? 'Salvage Title' : condition === 'N' ? 'New' : 'N/A'}
-  </div>
-);
+const Condition = ({ condition }: ConditionProp) => {
+  const getCondition = () => {
+    switch (condition) {
+      case 'A':
+        return 'Salvage Title';
+      case 'N':
+        return 'New';
+      default:
+        return 'N/A';
+    }
+  };
+
+  return <div>{getCondition()}</div>;
+};
 
 export default Condition;
