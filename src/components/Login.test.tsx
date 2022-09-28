c975a77e1fe9d4f9fbb0a0bdc531dc66a16dc9e1
Diana Calderon <dianacalderon@ravn.co>
2022-09-28 09:36:57 -0600

    Create Login test file

---
+++ src/components/Login.test.tsx
@@ -0,0 +1,93 @@
+import { MockedResponse } from '@apollo/client/testing';
+import '@testing-library/jest-dom';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+
+import { UserDocument } from '../generated/graphql';
+import {
+  invalidEmailUser,
+  nonExistentEmailUser,
+  testUser,
+} from '../test/dataTest';
+import { render } from '../test/testProvider';
+import { renderUserContext } from '../test/testUserContextProvider';
+import Header from './Header';
+import Login from './Login';
+
+const mockUser: MockedResponse<Record<string, any>>[] = [
+  {
+    request: {
+      query: UserDocument,
+      variables: { where: { email: { _eq: `${testUser.email}` } } },
+    },
+    result: {
+      data: {
+        users: [testUser],
+      },
+    },
+  },
+];
+
+const mockWrongEmailUser: MockedResponse<Record<string, any>>[] = [
+  {
+    request: {
+      query: UserDocument,
+      variables: { where: { email: { _eq: `${nonExistentEmailUser}` } } },
+    },
+    result: {
+      data: {
+        users: [],
+      },
+    },
+  },
+];
+
+describe('Test in Login Component', () => {
+  it('Should show the email required message', async () => {
+    render(<Login />, mockUser);
+    fireEvent.click(screen.getByText('login'));
+    expect(
+      await screen.findByText('email is a required field')
+    ).toBeInTheDocument();
+  });
+
+  it('Should show the invalid email message', async () => {
+    render(<Login />, mockUser);
+
+    fireEvent.change(screen.getByTestId('input-login'), {
+      target: { value: `${invalidEmailUser}` },
+    });
+    fireEvent.click(screen.getByText('login'));
+    expect(await screen.findByText('Invalid Email')).toBeInTheDocument();
+  });
+
+  it("Should show email doesn't exist message", async () => {
+    render(<Login />, mockWrongEmailUser);
+
+    fireEvent.change(screen.getByTestId('input-login'), {
+      target: { value: `${nonExistentEmailUser}` },
+    });
+    fireEvent.click(screen.getByText('login'));
+    expect(
+      await screen.findByText("This email doesn't exist")
+    ).toBeInTheDocument();
+  });
+
+  it('Should show welcome message', async () => {
+    render(<Login />, mockUser);
+    renderUserContext(<Header />, []);
+
+    fireEvent.change(screen.getByTestId('input-login'), {
+      target: { value: `${testUser.email}` },
+    });
+
+    fireEvent.click(screen.getByText('login'));
+
+    await waitFor(() => {
+      expect(screen.getByText('loading...')).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('greeting-user')).toBeInTheDocument();
+    });
+  });
+});
