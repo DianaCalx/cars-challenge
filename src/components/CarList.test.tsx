5d635438b072344d4e2a1b3f22b6e0116a6de700
Diana Calderon <dianacalderon@ravn.co>
2022-09-28 09:38:37 -0600

    Add test to CarList Component

--- src/components/CarList.test.tsx
+++ src/components/CarList.test.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable testing-library/no-wait-for-side-effects */
 import { MockedResponse } from '@apollo/client/testing';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import { GraphQLError } from 'graphql';
 
-import { CarsDocument } from '../generated/graphql';
-import { carsDataTest } from '../test/dataTest';
+import { CarsDocument, FavoritesDocument } from '../generated/graphql';
+import { carsDataTest, favoritesCars } from '../test/dataTest';
 import { render } from '../test/testProvider';
 import CarsList from './CarsList';
 
@@ -84,6 +85,43 @@ const mocksError: MockedResponse<Record<string, any>>[] = [
   },
 ];
 
+const mocksCarsFavorites: MockedResponse<Record<string, any>>[] = [
+  {
+    request: {
+      query: CarsDocument,
+      variables: {
+        whereCars: {
+          _or: [
+            {
+              title: {},
+            },
+            {
+              vin: {},
+            },
+          ],
+        },
+        orderBy: {},
+      },
+    },
+    result: {
+      data: {
+        cars: [carsDataTest[0]],
+      },
+    },
+  },
+  {
+    request: {
+      query: FavoritesDocument,
+      variables: { whereUserCars: { user_id: { _eq: 7 } } },
+    },
+    result: {
+      data: {
+        user_cars: favoritesCars,
+      },
+    },
+  },
+];
+
 describe('Test in CarList Component', () => {
   it('Should show the cars', async () => {
     render(<CarsList favorites={[]} setFavorites={() => {}} />, mocksCars);
@@ -101,5 +139,29 @@ describe('Test in CarList Component', () => {
   it('Should show an error message', async () => {
     render(<CarsList favorites={[]} setFavorites={() => {}} />, mocksError);
     expect(await screen.findByText('There was an error')).toBeInTheDocument();
+  });
+
+  it('Should show a fill star', async () => {
+    render(
+      <CarsList favorites={[236]} setFavorites={() => {}} />,
+      mocksCarsFavorites
+    );
+
+    expect(
+      await screen.findByText(`${carsDataTest[0].title}`)
+    ).toBeInTheDocument();
+
+    expect(screen.getByTestId('fill-star')).toBeInTheDocument();
+  });
+
+  it('Should show a outline star', async () => {
+    render(
+      <CarsList favorites={[]} setFavorites={() => {}} />,
+      mocksCarsFavorites
+    );
+    expect(
+      await screen.findByText(`${carsDataTest[0].title}`)
+    ).toBeInTheDocument();
+    expect(screen.getByTestId('outline-star')).toBeInTheDocument();
   });
 });
